43f147ef2fa5f063ae565933db0e6f2fd574cec7
nick076 <nickdekoning@ziggo.nl>
2016-05-13 14:17:25 +0200

    Auth System

--- personage/app/Http/routes.php
+++ personage/app/Http/routes.php
@@ -7,10 +7,13 @@
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| and give it the ntroller to call when that URI is requested.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+Route::auth();
+
+Route::get('/', 'HomeController@index');
