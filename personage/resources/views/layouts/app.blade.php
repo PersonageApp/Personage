31a1e58d4df1d4c4b46e2be129189d331d2e2d5d
nick076 <nickdekoning@ziggo.nl>
2016-05-23 12:14:51 +0200

    changes

--- personage/resources/views/layouts/app.blade.php
+++ personage/resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="http://i68.tinypic.com/2mwyqgl.png">
 
-    <title>Laravel</title>
+    <title>Personage Applicatie</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
