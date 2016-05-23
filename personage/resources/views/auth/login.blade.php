b960b35c8fb23303850df03ff6e00a5ef10e59ac
nick076 <nickdekoning@ziggo.nl>
2016-05-23 15:55:44 +0200

    Login style veranderingen

--- personage/resources/views/auth/login.blade.php
+++ personage/resources/views/auth/login.blade.php
@@ -1,66 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
+    <div class="container">
+        <div class="col-md-6 col-md-push-3" style="background-color: #4e5d6c; padding: 25px; border-radius: 4px;">
+            <div class="col-md-3" style="padding: 0px !important;">
+                <img src="{{ URL::asset('images/user.png') }}" style="width: 100%;">
+            </div>
+            <div class="col-md-9" style="padding: 0px 0px 0px 15px !important;">
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                    <div class="col-md-8">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}"> 
+                            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="E-mailadres">
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>   
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
+                            <input type="password" class="form-control" name="password" placeholder="Wachtwoord">
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div> 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember"> Remember Me
+                                </label>
                             </div>
                         </div>
-
+                    </div>
+                    <div class="col-md-4">
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-btn fa-sign-in"></i>Login
+                            </button>
+                        </div>    
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
