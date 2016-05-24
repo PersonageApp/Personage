0649023a4a9bbca4b287e05268f5c4bee920d4ef
nick076 <nickdekoning@ziggo.nl>
2016-05-24 15:54:19 +0200

    home.blade.php

--- personage/resources/views/home.blade.php
+++ personage/resources/views/home.blade.php
@@ -8,7 +8,25 @@
                 <div class="panel-heading">Voeg uw verhaal toe</div>
 
                 <div class="panel-body">
-                    Nick is een sukkel en jesse weet hoe hij waar deze pagina staat.
+                <table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>naam</th>
+      <th>verhaal</th>
+    </tr>
+  </thead>
+  <tbody>
+                    <?php DB::table('verhalen')->orderBy('verhaal_id')->chunk(100, function($users) {
+    foreach ($users as $user) {
+        echo '
+    <tr>
+      <td>'; echo $user->verhaal_id; echo'</td>
+      <td>'; echo $user->naam; echo'</td>
+      <td>'; echo $user->verhaal; echo'</td>
+    </tr>';
+    }
+}); ?></tbody></table>
                     <form action="#">
                         <input type="text" class="form-control">
                         <input type="submit" class="btn-primary">
