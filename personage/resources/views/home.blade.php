0da8831b666812ff7389282fe78a435524106551
Jessepeffer <jesse1995peffer@hotmail.com>
2016-05-24 15:41:05 +0200

    dashboard change

--- personage/resources/views/home.blade.php
+++ personage/resources/views/home.blade.php
@@ -5,10 +5,14 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Voeg uw verhaal toe</div>
 
                 <div class="panel-body">
                     Nick is een sukkel en jesse weet hoe hij waar deze pagina staat.
+                    <form action="#">
+                        <input type="text" class="form-control">
+                        <input type="submit" class="btn-primary">
+                    </form>
                 </div>
             </div>
         </div>
