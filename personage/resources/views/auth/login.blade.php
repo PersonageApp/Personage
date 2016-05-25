393084be63fbd9e1f52ebfe60bf87749bd7fc418
nick076 <nickdekoning@ziggo.nl>
2016-05-25 10:49:55 +0200

    inDown

--- personage/resources/views/auth/login.blade.php
+++ personage/resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container">
-    <span id="danger"><span class="dd animated bounce">
+    <span id="danger"><span class="dd animated bounceInDown">
         <div class="col-md-6 col-md-push-3" style="background-color: #4e5d6c; padding: 25px; border-radius: 4px;">
             <div class="col-md-3" style="padding: 0px !important;">
                 <img src="{{ URL::asset('images/user.png') }}" style="width: 100%;">
