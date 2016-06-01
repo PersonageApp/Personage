f101bd5fe24a4ac081f096c9d1896ed5fbc3044b
nick076 <nickdekoning@ziggo.nl>
2016-06-01 15:46:04 +0200

    Aanpassingen

--- personage/resources/views/layouts/app.blade.php
+++ personage/resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}"></a></li>
+                    
                 </ul>
 
                 <!-- Right Side Of Navbar -->
--- personage/resources/views/verhaal/bekijken.blade.php
+++ personage/resources/views/verhaal/bekijken.blade.php
@@ -2,13 +2,41 @@
 
 @section('content')
     <div class="container">
-    <h1>Bekijk het verhaal</h1>
-    <div class="panel panel-default">
-        <div class="panel-heading">Verhaal schrijven?</div>
-        <div class="panel-body">
-            <h2>{{ $verhaal[0]->naam }}</h2> 
-            <p>{{ $verhaal[0]->verhaal}}</p>
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#verhaal" data-toggle="tab" aria-expanded="true">Verhaal</a></li>
+            <li class=""><a href="#werelden" data-toggle="tab" aria-expanded="false">Werelden</a></li>
+            <li class=""><a href="#locaties" data-toggle="tab" aria-expanded="false">Locaties</a></li>
+            <li class=""><a href="#families" data-toggle="tab" aria-expanded="false">Families</a></li>
+            <li class=""><a href="#personages" data-toggle="tab" aria-expanded="false">Personages</a></li>
+            <li class="gaterug"><a href="{{ url('/') }}" >Ga terug</a></li>
+        </ul>
+        <div id="myTabContent verhalentab" class="tab-content">
+            <div class="tab-pane fade active in" id="verhaal">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Het verhaal <p style="float: right">Verhaal: {{ $verhaal[0]->naam }}</p></div>
+                    <div class="panel-body">
+                        <h2>{{ $verhaal[0]->naam }}</h2> 
+                        <p>{{ $verhaal[0]->verhaal}}</p>
+                    </div>
+                </div>  
+            </div>
+            <div class="tab-pane fade" id="werelden">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Wereld toevoegen <p style="float: right">Verhaal: {{ $verhaal[0]->naam }}</p></div>
+                    <div class="panel-body">
+                        test
+                    </div>
+                </div>             
+            </div>
+            <div class="tab-pane fade" id="locaties">
+                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.</p>
+            </div>
+            <div class="tab-pane fade" id="families">
+                <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater.</p>
+            </div>
+            <div class="tab-pane fade" id="personages">
+                <p>Tnd seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater.</p>
+            </div>
         </div>
-    </div>  
     </div>  
 @stop
